04ea65426b3658b34240f2a6bcfe46a47d2ecaa5
Amit Kumar <alsoamit@gmail.com>
2024-09-25 20:34:20 +0530

    feat: enable sqs message deletion

--- trigger.py
+++ trigger.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     # Define the message parameters
     message = {
         "bucket": "syneurgy-prod",
-        "key": "meetings/9e39be7b-1bbe-4673-bb3c-aa6bdd48caa5/trust.mp4",
-        "meetingId": "9e39be7b-1bbe-4673-bb3c-aa6bdd48caa5",
+        "key": "meetings/5109473a-b19b-4513-b6e1-c41f7dc9e5d7/trust.mp4",
+        "meetingId": "5109473a-b19b-4513-b6e1-c41f7dc9e5d7",
         "type": "analysis"
     }
 
